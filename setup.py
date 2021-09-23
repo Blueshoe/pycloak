36a3043d4d1f60302f3e144d0bec0fd23da8d4f7
veit <veit@blueshoe.de>
2021-09-23 16:28:19 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import io
+from os.path import exists
+from setuptools import setup, find_packages
+
+__version__ = "0.0.0"
+
+setup(
+    name='pycloak',
+    version=__version__,
+    author='Blueshoe',
+    author_email='veit@blueshoe.de',
+    description='Utils around keycloak and other oidc clients.',
+    long_description=io.open('README.md', encoding='utf-8').read() if exists("README.md") else "",
+    packages=find_packages(),
+    install_requires=[
+        'Django>=2.2',
+        'PyJWT~=2.1.0',
+    ],
+    python_requires='>=3.8',
+    include_package_data=True,
+    scripts=[],
+    url='https://gitlab.blueshoe.de/loyalty-engine/client-sdk',
+    license='Copyright',
+    classifiers=[
+        # 'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        # 'License :: OSI Approved :: MIT License',
+        'Framework :: Django',
+        'Framework :: Django :: 2.2',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    zip_safe=False,
+)
