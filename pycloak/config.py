037709cdca1437793fdc4b0ef66ae7c6f127b01d
Veit Rückert <veit@blueshoe.de>
2022-02-10 17:51:08 +0100

    fix: lazy load conf
    Let conf access settings lazily

--- pycloak/config.py
+++ pycloak/config.py
@@ -2,26 +2,30 @@ from django.conf import settings
 
 
 class _PycloakConfiguration:
-    def __init__(self):
-        self.ALGORITHM = getattr(settings, "PYCLOAK_ALGORITHM", None)
-        self.AUDIENCE = getattr(settings, "PYCLOAK_AUDIENCE", None)
-        self.PUBLIC_KEY = getattr(settings, "PYCLOAK_PUBLIC_KEY", None)  # can be exported from keyclaok realm
-        self.TOKEN_HEADER = getattr(settings, "PYCLOAK_TOKEN_HEADER", "HTTP_X_FORWARDED_ACCESS_TOKEN")
-        self.CLIENT_ID = getattr(settings, "PYCLOAK_CLIENT_ID", "account")
-        self.ALLOW_DEFAULT_LOGIN = getattr(settings, "PYCLOAK_ALLOW_DEFAULT_LOGIN", False)
-        self.USERNAME_CLAIM = getattr(settings, "PYCLOAK_USERNAME_CLAIM", "preferred_username")
-        self.FIRSTNAME_CLAIM = getattr(settings, "PYCLOAK_FIRSTNAME_CLAIM", "given_name")
-        self.LASTNAME_CLAIM = getattr(settings, "PYCLOAK_LASTNAME_CLAIM", "family_name")
-        self.EMAIL_CLAIM = getattr(settings, "PYCLOAK_EMAIL_CLAIM", "email")
-        self.STAFF_ROLES = getattr(settings, "PYCLOAK_STAFF_ROLES", [])
-        self.SUPERUSER_ROLES = getattr(settings, "PYCLOAK_SUPERUSER_ROLES", [])
-        self.TOKENID_CLAIM = getattr(settings, "PYCLOAK_TOKENID_CLAIM", "jti")
-        self.SESSION_KEY = getattr(settings, "PYCLOAK_SESSION_KEY", "_pycloak_token_id")
+    DEFAULTS = {
+        "ALGORITHM": None,
+        "AUDIENCE": None,
+        "PUBLIC_KEY": None,  # can be exported from keyclaok realm
+        "TOKEN_HEADER": "HTTP_X_FORWARDED_ACCESS_TOKEN",
+        "CLIENT_ID": "account",
+        "ALLOW_DEFAULT_LOGIN": False,
+        "USERNAME_CLAIM": "preferred_username",
+        "FIRSTNAME_CLAIM": "given_name",
+        "LASTNAME_CLAIM": "family_name",
+        "EMAIL_CLAIM": "email",
+        "STAFF_ROLES": [],
+        "SUPERUSER_ROLES": [],
+        "TOKENID_CLAIM": "jti",
+        "SESSION_KEY": "_pycloak_token_id",
+    }
+
+    def __getattr__(self, item):
+        if item in self.DEFAULTS:
+            return getattr(settings, f"PYCLOAK_{item}", self.DEFAULTS[item])
+        raise AttributeError
 
     def to_dict(self):
-        return {
-            k: v for k, v in self.__dict__.items() if k.isupper()
-        }
+        return {k: getattr(self, k) for k in self.DEFAULTS}
 
     def __str__(self):
         return str(self.to_dict())
